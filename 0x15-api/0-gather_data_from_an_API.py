222ed3bc53a17acfcef789a12dad7baf9656bde9
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-25 21:11:16 +0100

    API Request in Python III

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,22 +10,23 @@ from http.client import HTTPResponse
 import requests
 from sys import argv
 
-USER_ID = argv[1]
-base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    USER_ID = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
 
-user_data = requests.get(f"{base_url}/users/{USER_ID}")
-todo_data = requests.get(f"{base_url}/users/{USER_ID}/todos")
+    user_data = requests.get(f"{base_url}/users/{USER_ID}")
+    todo_data = requests.get(f"{base_url}/users/{USER_ID}/todos")
 
-user_data = user_data.json()
-todo_data = todo_data.json()
+    user_data = user_data.json()
+    todo_data = todo_data.json()
 
-num_of_tasks = len(todo_data)
-completed_tasks = len([cmpl for cmpl in todo_data
-                       if cmpl["completed"]])
+    num_of_tasks = len(todo_data)
+    completed_tasks = len([cmpl for cmpl in todo_data
+                        if cmpl.get("completed")])
 
-# Print formatted output as on task
-print("Employee {} is done with tasks({}/{}):"
-      .format(user_data.get("name", ""), completed_tasks, num_of_tasks))
+    # Print formatted output as on task
+    print("Employee {} is done with tasks({}/{}):"
+        .format(user_data.get("name", ""), completed_tasks, num_of_tasks))
 
-[print("\t {}".format(task["title"])) for task in todo_data
- if task["completed"]]
+    [print("\t {}".format(task["title"])) for task in todo_data
+    if task.get("completed")]
