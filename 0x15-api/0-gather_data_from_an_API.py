d6d12269a952fec6dd0ef7c2796924f16424af24
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-25 20:57:37 +0100

    API Request in Python

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Write a Python script that, using this REST API
+[https://jsonplaceholder.typicode.com/], for a given
+employee ID, returns information about his/her
+TODO list progres
+"""
+
+import requests
+from http.client import HTTPResponse
+from sys import argv
+
+USER_ID = argv[1]
+base_url = "https://jsonplaceholder.typicode.com"
+
+user_data = requests.get(f"{base_url}/users/{USER_ID}")
+todo_data = requests.get(f"{base_url}/users/{USER_ID}/todos")
+
+user_data = user_data.json()
+todo_data = todo_data.json()
+
+num_of_tasks = len(todo_data)
+completed_tasks = len([cmpl for cmpl in todo_data
+                       if cmpl["completed"]])
+
+# Print formatted output as on task
+print("Employee {} is done with tasks({}/{}):"
+      .format(user_data.get("name", ""), completed_tasks, num_of_tasks))
+
+[print("\t {}".format(task["title"])) for task in todo_data
+ if task["completed"]]
---
+++ 0x15-api/README.md
@@ -0,0 +1,11 @@
+# API Repository
+
+This repository contains tasks and projects related to API (Application Programming Interface) development.
+
+## Table of Contents
+
+- [Description](#description)
+
+## Description
+
+In this repository, you will find various tasks and projects that focus on API development. Each task or project is organized in its own directory and contains a README file with specific instructions and requirements.
